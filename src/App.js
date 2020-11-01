9e7156b12fa4f4d8822d9046efe3ef33ee739bed
Ana Nikolova <ani.nik19@gmail.com>
2020-11-01 13:11:19 +0100

    fetched the data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import React, { Component } from "react";
 import Cards from "./components/Cards/Crads";
 import Chart from "./components/Charts/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
+import { fetchData } from "./api";
 
 import styles from "./App.module.css";
 
 class App extends Component {
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
   render() {
     return (
       <div className={styles.container}>
